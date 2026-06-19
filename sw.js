aededb47c0488e304a34d71d4cdd84f3978e34a4
SANECA MACK <SANECATV@GMAIL.COM>
2026-06-18 20:01:35 -0400

    Rename cache from 'stv-gold-v1' to 'xoco-base-v1'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'stv-gold-v1';
+const CACHE = 'xoco-base-v1';
 const ASSETS = ['./', './index.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
